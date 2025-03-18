bd571b255f32e52c335f7ffecdc1da9649796f26
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-03-18 10:03:27 +0200

    finished the project, the saving and profile features coming soon

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -31,6 +31,11 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        // Call updateSearchCount only if there are results
+        if (movies?.length! > 0 && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies[0]);
+        }
       } else {
         reset();
       }
@@ -38,13 +43,6 @@ const Search = () => {
 
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
-
-  useEffect(() => {
-      // Call updateSearchCount only if there are results
-      if (movies?.length! > 0 && movies?.[0]) {
-        updateSearchCount(searchQuery, movies[0]);
-      }
-  }, [movies]);
 
   return (
     <View className="flex-1 bg-primary">
